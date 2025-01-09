325bd1b81475833afef178097feabdfc16251440
Hedr1ch <hedr1ch@outlook.dk>
2025-01-09 12:08:34 +0100

    3rd push

--- Readme.txt
+++ Readme.txt
@@ -3,22 +3,38 @@ This script is able to pull folders and files remote to a local destination.
 
 
 [How to]
+For this script to be able to run proberly. Pssession has to be enabled on both devices. To do this run the following command.
+
+Enable-PSRemoting -Force
+
+If the devices are not domain joined, they might not trust eachother. Run the following commands, to set trusted ip.
+
+Set-Item WSMan:localhost\client\trustedhosts -value ""
+Get-Item WSMan:localhost\client\trustedhosts
+
 The config.json file has to be edited to be able to work proberly.
 
-"localDestination": ".\\Backup"
-    This variable is where the backup is stored local (Default: .\\Backup)
+"sourcePath": "C:\\Temp"
+    This variable is the remote destination that you want to copy (Default: C:\\Temp)
 
 "logDestination": ".\\Log"
     This variable is where the log is stored local and the name (Default: .\\Log)
     *IMPORTANT* If the script is run again, it will overwrite the exitsing log file. Please change the name, if the old file needs to be saved.
-    
-"remoteDestination": "C:\\Temp"
-    This variable is the remote destination that you want to copy (Default: C:\\Temp)
 
-"backupName":  "ServerBackup"
-    This variable is the name of the backup folder (Default: ServerBackup)
-    *IMPORTANT* If the script is run again, it will overwrite the exitsing backup file. Please change the name, if the old file needs to be saved.
+"destinationPath": ".\\Backup"
+    This variable is where the backup is stored local (Default: .\\Backup)
+    *IMPORTANT* If the script is run again, it will overwrite the exitsing backup folder. Please change the name, if the old file needs to be saved.
+
+"destinationIP": ""
+    This variable contains the ip of the destination, where you want the backup to be (Example: "192.168.0.1")
+
+"remoteIP": ""
+    This variable contains the ip of the remote host, where the files you want a backup from exist (Example: "192.168.0.1")
+
+The script will ask for login credintals. When prompted please enter credintals with administrator rights.
+
+For more infomation please check log.
 
 
 [Version]
-Release version 0.2 (08-01-2025) by Nicolas Hedrich
+Release version 1.0 (09-01-2025) by Nicolas Hedrich
--- serverCaseBackup.ps1
+++ serverCaseBackup.ps1
@@ -8,12 +8,12 @@
     This script is able to pull folders and files remote to a local destination.
 
 .EXAMPLE
-
+    You need to do a backup daily on a remote server. This script will be able to do this, and store the files local.
 
 .NOTES
     Author: Nicolas Hedrich
-    Version: 0.2
-    Release date: 08-01-2025
+    Version: 1.0
+    Release date: 09-01-2025
 #>
 
 # Config.json location and translation.
@@ -21,10 +21,11 @@ $configFilePath = ".\Config.json" # Variable that contains Config.json path.
 $configContent = Get-Content -Path $configFilePath -Raw | ConvertFrom-Json # Makes the script able to read and translate the Config.json file.
 
 # Config.json content.
-$localDestination = $configContent.localDestination
+$sourcePath = $configContent.sourcePath
 $logDestination = $configContent.logDestination
-$remoteDestination = $configContent.remoteDestination
-$backupName = $configContent.backupName
+$destinationPath = $configContent.destinationPath
+$destinationIP = $configContent.destinationIP
+$remoteIp = $configContent.remoteIP
 
 # Main script function.
 function RemoteBackup {
@@ -34,16 +35,15 @@ function RemoteBackup {
     Clear-Host
 
     # Checks if folder exist. If folder doesn't exist it will be created.
-    if (-Not (Test-Path -Path $localDestination)) { 
-        New-Item -ItemType Directory -Path $localDestination
+    if (-Not (Test-Path -Path $destinationPath)) { 
+        New-Item -ItemType Directory -Path $destinationPath
         Clear-Host
     }
 
+    # While loop, that makes sure not to come any further in the script, before a connection can be established.
     while ($true) {
 
-        # Variables that contains IP and credentials. <#
-        $remoteIp = Read-Host "Enter IP of destiantion host.`n"
-        Clear-Host 
+        # Variables that contains credentials.
         $username = Read-Host "Enter username for remote host (Has to have administrative rights).`n"
         Clear-Host 
         $password = Read-Host "Enter password of user.`n"
@@ -51,13 +51,15 @@ function RemoteBackup {
         $securePassword = ConvertTo-SecureString $password -AsPlainText -Force 
         $credential = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
 
+        # Try to connect to the remote server with the given ip and credentials.
         try { 
             $session = New-PSSession -ComputerName $remoteIp -Credential $credential
-            Write-Host "Connection to remote host was established." 
-            Pause
-            Break
+            Remove-PSSession -Session $session
             Clear-Host
+            Break
         } 
+
+        # If the connection is unsuccesful. The script will ask for credentials again.
         catch { 
             Write-Host "Could not reach host. Try again."
             Pause
@@ -65,10 +67,13 @@ function RemoteBackup {
         }
     }
 
-    robocopy \\127.0.0.1\c$\temp \\192.168.185.240$localDestination /E /Z /ZB /R:5 /W:5 /TBD /V /MT:16
-    Read-Host = "`n`nPress any key to close script"
+    # When the script knows it can establish a connection. It will invoke the backup procedure.
+    Invoke-Command -ComputerName $remoteIp -Credential $credential -ScriptBlock {
+        param ($sourcePath, $destinationPath, $destinationIP)
+        robocopy $sourcePath \\$destinationIP\$destinationPath /E /MIR /MT16
+    } -ArgumentList $sourcePath, $destinationPath, $destinationIP
 
-    Remove-PSSession -Session $session
+    Read-Host = "`n`nPress any key to close script"
 
     # Stop logging.
     Stop-Transcript
